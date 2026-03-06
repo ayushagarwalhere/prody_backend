5437f23f9d75c31e689986be2d5e1ff26d8219d2
ayushagarwalhere <ayush.agarwal.ayush.21@gmail.com>
2026-03-06 20:17:13 +0530

    auth-controller added

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
 
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
+app.use(cookieParser());
+app.use(cors());
 
-app.use
-
-
-app.get('/check', (req,res) => {
-    res.status(200).json({ status: "positive" })
-})
+app.get("/check", (req, res) => {
+  res.status(200).json({ status: "positive" });
+});
 
 app.listen(port, () => {
-    console.log("Server started at http://localhost:3000")
-})
+  console.log("Server started at http://localhost:3000");
+});
